943aa2437ee62a2f58d664ab0abf7755ee6298ba
chutki12 <priyankachaudhary2068>
2022-08-03 23:58:58 +0530

    ADDED : Welcome To User Registration Program

---
+++ main/java/com/bridgelabz/userRegistrationJUnit/UserRegistration.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.userRegistrationJUnit;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		 
+		System.out.println(" Welcome To User Registration Program");
+
+	}
+
+}
