4be79d6796b5f113475844250b04f111f76c9ba5
chutki12 <priyankachaudhary2068>
2022-08-04 01:06:51 +0530

    ADDED : This Code For To Refactor The Code To Throw Custom Exceptions For Invalid User Details

--- main/java/com/bridgelabz/userRegistrationJUnit/UserRegistration.java
+++ main/java/com/bridgelabz/userRegistrationJUnit/UserRegistration.java
@@ -6,32 +6,46 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	
 
-	    private static final String NAME_REGEX = "[A-Z][a-z]{3,}";
+	private static final String NAME_REGEX = "[A-Z][a-z]{2,}";
 
-	    public boolean validateFirstName(String firstName) {
-	        return Pattern.matches(NAME_REGEX, firstName);
-	    }
+    public boolean validateFirstName(String firstName) {
+        if (Pattern.matches(NAME_REGEX, firstName)) {
+            return true;
+        } else {
+            throw new UserRegistrationException("Invalid first name");
+        }
+    }
 
-	    public boolean validateLastName(String lastName) {
-	        return Pattern.matches(NAME_REGEX, lastName);
-	    }
+    public boolean validateLastName(String lastName) {
+        if (Pattern.matches(NAME_REGEX, lastName)) {
+            return true;
+        } else {
+            throw new UserRegistrationException("Invalid last name");
+        }
+    }
 
-	    public boolean validateEmailId(String emailId) {
-	        return Pattern.matches("[a-z][A-Z a-z 0-9]+[@][a-z]+[.][a-z]{2,}", emailId);
-	    }
+    public boolean validateEmailId(String emailId) {
+        if (Pattern.matches("[a-z][A-Z a-z 0-9]+[@][a-z]+[.][a-z]{2,}", emailId)) {
+            return true;
+        } else {
+            throw new UserRegistrationException("Invalid Email");
+        }
+    }
 
-	    public boolean validateMobileNumber(String mobileNumber) {
-	        return Pattern.matches("[+]91 [6-9]\\d{9}", mobileNumber);
-	    }
+    public boolean validateMobileNumber(String mobileNumber) {
+        if (Pattern.matches("[+]91 [6-9]\\d{9}", mobileNumber)) {
+            return true;
+        } else {
+            throw new UserRegistrationException("Invalid mobile number");
+        }
+    }
 
-	    public boolean validatePassword(String password) {
-	        return Pattern.matches("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}", password);
-	    }
-
-	    public boolean validateMultipleEmailId(String multipleEmailId) {
-	        return Pattern.matches("[a-z \\d -.]+[@][a-z \\d]+[.][a-z]{3,}", multipleEmailId);
-	        
-	    }
-	}
-
+    public boolean validatePassword(String password) {
+        if (Pattern.matches("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}", password)) {
+            return true;
+        } else {
+            throw new UserRegistrationException("Invalid Password");
+        }
+    }
+}
 
---
+++ main/java/com/bridgelabz/userRegistrationJUnit/UserRegistrationException.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.userRegistrationJUnit;
+
+
+public class UserRegistrationException extends RuntimeException {
+	
+	 
+	    public UserRegistrationException(String message) {
+	        super(message);
+	    }
+	}
+
+
